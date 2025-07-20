ea6e08f923153fa29ef99a04a5aa3602a582e8fb
ishanpuri97-dotcom <ishanpuri97@gmail.com>
2025-07-21 03:26:20 +0530

    Add .next/static/chunks/src_app_dashboard_hr_page_tsx_cf837448._.js 1753048580299

---
+++ .next/static/chunks/src_app_dashboard_hr_page_tsx_cf837448._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_033f8a6a._.js",
+  "static/chunks/node_modules_19c3c2ac._.js"
+],
+    source: "dynamic"
+});
